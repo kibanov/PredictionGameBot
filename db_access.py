51030979ed12a310f07811bfd6146d32f690869e
Mark Kibanov <kibanov@cs.uni-kassel.de>
2016-06-09 00:13:52 +0200

    First draft of working version.

---
+++ db_access.py
@@ -0,0 +1,68 @@
+import pymongo
+
+from pymongo import MongoClient
+client = MongoClient()
+client = MongoClient('localhost', 27017)
+eurodb = client.euro16
+predictions_collection = eurodb.predictions
+matches_collection = eurodb.matches
+
+# ADD USER:
+def add_user(uid):
+    user_search = predictions_collection.count({"user_id": uid})
+    if user_search == 0:
+        post = {"user_id": uid}
+        user_id = predictions_collection.insert_one(post).inserted_id
+    else:
+        existing_user = predictions_collection.find_one({"user_id": uid})
+        user_id = existing_user.get('_id')
+    return (user_id)
+
+def add_prediction(uid, matchid, winner, goals, total):
+    result = predictions_collection.update_one({"user_id": uid}, {"$addToSet": {"predicted_matches" : matchid}})
+    result2 = predictions_collection.update_one(
+    	{"user_id": uid},
+    	{
+    	    "$addToSet": {
+    	        "predictions":
+                    { "match_no": matchid,
+                    "winner": winner,
+                    "goals": goals,
+                    "total": total}
+            }
+        }, upsert=True)
+    return (result)
+
+
+def add_name(uid, uname):
+	result = predictions_collection.update_one(
+		{"user_id": uid},
+		{"$set": {"name" : uname}})
+
+def add_group(uid, group):
+    result = predictions_collection.update_one(
+        {"user_id": uid},
+        {"$addToSet": {"groups" : group}})
+
+def get_next_match(uid):
+    active_matchs = list(matches_collection.find({"active" : 1},{"match_no" : 1, "_id" : 0}))
+    predicted_matches = list(predictions_collection.find({"user_id" : uid},{"predicted_matches" : 1, "_id" : 0}))
+    active_match_ids = [c['match_no'] for c in active_matchs]
+    if (len(predicted_matches[0]) > 0):
+        relevant_matches = [c for c in active_match_ids if c not in predicted_matches[0]['predicted_matches']]
+    else:
+    	relevant_matches = active_match_ids
+    next_match_id = sorted(relevant_matches)[0]
+    next_match = list(matches_collection.find({"match_no" : next_match_id},{"_id" : 0}))
+    return(next_match)
+
+# UPDATE USER:
+def update_user(id):
+    return ()
+
+# ADD PREDICTION:
+# def add_prediction(user_id, match_no, winner, goals, total):
+    # conncet to mongo
+    # check if the 
+
+# EDIT PREDICTION:
--- echo_bot.py
+++ echo_bot.py
@@ -1,36 +1,145 @@
 import telebot
 import yaml
+from telebot import types
+
+import db_access
 
 config = yaml.safe_load(open("conf.yml"))
 
 bot = telebot.TeleBot(config["token"])
 
+team1 = ''
+team2 = ''
+
+class Prediction:
+    def __init__(self, uid):
+        self.userid = uid
+        self.matchid = None
+        self.win = None
+        self.goal = None
+        self.total = None
+
+pred = Prediction(1)
+
+
 @bot.message_handler(commands=['start'])
-def send_welcome(message):
+def send_start(message):
     try:
         chat_id = message.chat.id
         bot.send_message(chat_id, "Welcome to Euro 2016 prediction contest! Type /help for help, /rules for rules or /predict to make your predictions")
+        db_access.add_user(chat_id)
     except Exception as e:
-        bot.reply_to(message, 'oooops')
+        bot.reply_to(message, 'Something went wrong!')
 
 @bot.message_handler(commands=['help'])
-def send_welcome(message):
+def send_help(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "/start View start message\n/help View this message\n/predict Make prediction")
+    bot.send_message(chat_id, '/start View start message\n' +
+        '/help View this message\n' +
+        '/predict Make prediction\n' +
+        '/setname Set your name\n' +
+        '/setgroup Set your group\n' + 
+        '/rules Show rules')
 
 @bot.message_handler(commands=['rules'])
-def send_welcome(message):
+def send_rules(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "You need to answer general questions (Who will win the tournament? Who will score the most goals?) And three questions about each match:\n1. Who will win the game or will there be a draw?\n2. What will be ")
+    bot.send_message(chat_id, "You need to answer three questions about each match:\n1. Who will win the game or will there be a draw?\n2. What will be the difference of team goals?\n3. Total of the match?\nYou get 3 points for each correct answer. If all 3 answers are correct, you get an additional point.")
+
+@bot.message_handler(commands=['setname'])
+def set_name(message):
+    msg = bot.reply_to(message, "What is your name?")
+    bot.register_next_step_handler(msg, process_name)
+
+def process_name(message):
+    chat_id = message.chat.id
+    name = message.text
+    db_access.add_name(chat_id, name)
+    bot.send_message(chat_id, "Nice to meet you, " + name + "!")
+
+@bot.message_handler(commands=['setgroup'])
+def set_group(message):
+    msg = bot.reply_to(message, "Type the name of the group you want to be added to:")
+    bot.register_next_step_handler(msg, process_group)
+
+def process_group(message):
+    chat_id = message.chat.id
+    group_name = message.text
+    db_access.add_group(chat_id, group_name)
+    bot.send_message(chat_id, "You were added to the group " + group_name + "!")
 
 @bot.message_handler(commands=['predict'])
-def send_welcome(message):
+def send_predict(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "We are currently working on this feature!")
+    global team1
+    global team2
+    global pred
+    pred = Prediction(chat_id)
+    next_match = db_access.get_next_match(chat_id)
+    pred.matchid = [c['match_no'] for c in next_match][0]
+    team1 = [c['team_1'] for c in next_match][0]
+    team2 = [c['team_2'] for c in next_match][0]
+    stage = [c['type'] for c in next_match][0]
+    if (stage == 'Group'):
+        stage = stage + ' ' + [c['group'] for c in next_match][0]
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    # db_access.add_prediction(chat_id, 2, 3, 4, 2)
+    team1button = types.KeyboardButton(team1)
+    team2button = types.KeyboardButton(team2)
+    drawbutton = types.KeyboardButton("Draw")
+    markup.add(team1button, team2button, drawbutton)
+    msg = bot.reply_to(message, stage + ': ' + team1 + ' vs. ' + team2 + '\n Who will win the game?', reply_markup=markup)
+    bot.register_next_step_handler(msg, process_win_step)
+    # bot.send_message(chat_id, "Prediction added")
 
+def process_win_step(message):
+    chat_id = message.chat.id
+    global pred
+    print(team1)
+    print(message.text)
+    if (message.text == team1):
+        pred.win = 1
+    elif (message.text == team2):
+        pred.win = 2
+    elif (message.text == u'Draw'):
+        pred.win = 0
+    else:
+        bot.send_message(chat_id, "Error!")
+    print(pred.win)
+    if (message.text == u'Draw'):
+        msg = bot.reply_to(message, "How many goals will each team score?")
+        bot.register_next_step_handler(msg, process_goal_step)
+    else:
+        msg = bot.reply_to(message, "What will be the goal difference?")
+        bot.register_next_step_handler(msg, process_goal_step)
 
-@bot.message_handler(func=lambda message: True)
-def echo_all(message):
-    bot.reply_to(message, message.text)
+    # bot.register_next_step_handler(msg, process_goal_step)
+
+def process_goal_step(message):
+    chat_id = message.chat.id
+    global pred
+    pred.goal = int(message.text)
+    underbutton = types.KeyboardButton('< 2.5')
+    overbutton = types.KeyboardButton('> 2.5')
+    markup = types.ReplyKeyboardMarkup(one_time_keyboard=True)
+    markup.add(underbutton, overbutton)
+    msg = bot.reply_to(message, 'How many goals will the teams score?', reply_markup=markup)
+    bot.register_next_step_handler(msg, process_total_step)
+
+def process_total_step(message):
+    chat_id = message.chat.id
+    global pred
+    if (message.text == '< 2.5'):
+        pred.total = 0
+    elif (message.text == '> 2.5'):
+        pred.total = 1
+    else:
+        msg = bot.send_message(chat_id, "Error!")
+    db_access.add_prediction(pred.userid, pred.matchid, pred.win, pred.goal, pred.total)
+    bot.send_message(chat_id, "Your prediction for this game is saved!")
+
+# @bot.message_handler(func=lambda message: True)
+# def echo_all(message):
+#     bot.reply_to(message, message.text)
 
 bot.polling()
