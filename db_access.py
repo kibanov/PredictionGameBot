c805c41ddb71c9388c1f9ca237b7069470fd7595
Mark Kibanov <kibanov@cs.uni-kassel.de>
2016-06-13 17:53:46 +0200

    * matches data added * points count added

--- db_access.py
+++ db_access.py
@@ -95,9 +95,83 @@ def today_matches():
     return(res)
 
 
+def get_match(mid):
+    matches = list(matches_collection.find({"match_no" : mid}))
+    if (len(matches) == 0):
+        return (0)
+    else:
+        return (matches[0])
+
+def get_prediction_points(prediction, match):
+    goals_team_1 = match['goals_team_1']
+    goals_team_2 = match['goals_team_2']
+    quote = match['quote']
+
+    pred_winner = prediction['winner']
+    pred_goals = prediction['goals']
+    pred_total = prediction['total']
+    points = 0
+    # Points for winner of the match:
+    if (pred_winner == 0 and goals_team_1 == goals_team_2):
+        points = points + 3
+    elif (pred_winner == 1 and goals_team_1 > goals_team_2):
+        points = points + 3
+    elif (pred_winner == 2 and goals_team_2 > goals_team_1):
+        points = points + 3
+
+    # Points for goal difference:
+    if (points == 3):
+        if (pred_winner == 0 and pred_goals == goals_team_1):
+            points = points + 3
+        elif (pred_winner != 0 and pred_goals == abs(goals_team_1 - goals_team_2)):
+            points = points +3
+
+    # Points for total:
+    if (pred_total == 0 and (goals_team_1 + goals_team_2) < 3):
+        points = points + 3
+    if (pred_total == 1 and (goals_team_1 + goals_team_2) > 2):
+        points = points + 3
+
+    # Bonus points:
+    if (points == 9):
+        points = 10
+
+    res = points * quote
+    return (res)
+
 # UPDATE USER:
-def update_user(id):
-    return ()
+def update_user_points(uid):
+    user = get_user(uid)
+    if ('predictions' in user):
+        user_predictions = user['predictions']
+        for prediction in user_predictions:
+            match_no = prediction['match_no']
+            match = get_match(match_no)
+            if (match['finished'] == 0):
+                points = 0
+            else:
+                points = get_prediction_points(prediction, match)
+
+            predictions_collection.update({"user_id" : uid, 
+                                            "predictions" : {"$elemMatch" : {"match_no" : match_no}}},
+                                            { "$set" : {"predictions.$.points" : points}})
+        user = get_user(uid)
+        user_predictions = user['predictions']
+        all_user_points = [c['points'] for c in user_predictions]
+        total_points = sum(all_user_points)
+    else:
+        total_points = 0
+    predictions_collection.update(  {"user_id" : uid},
+                                    {"$set" : {"total_points" : total_points}})
+    return (total_points)
+
+def update_all_users_points():
+    users = get_all_users_ids()
+    # for user in users:
+    #     a = update_user_points(user)
+    users_points = map(update_user_points, users)
+    return([u for u in users_points])
+
 
 # ADD PREDICTION:
 # def add_prediction(user_id, match_no, winner, goals, total):
---
+++ refresh_points.py
@@ -0,0 +1,8 @@
+import db_access
+
+def main():
+    db_access.update_all_users_points()
+
+
+if __name__ == '__main__':
+    main()
