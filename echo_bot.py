61ee2c105bb94f2b60b7a8b378affaf9a8829c80
Mark Kibanov <kibanov@cs.uni-kassel.de>
2016-06-18 20:04:53 +0200

    improved PEP8 compatibility fixed /current command refreshed matches data

--- echo_bot.py
+++ echo_bot.py
@@ -1,6 +1,7 @@
 import telebot
 import yaml
 import sys
+
 from telebot import types
 
 import db_access
@@ -26,39 +27,67 @@ class Match:
         self.team2 = teamname2
 
 
+#################### FUNCTIONS TO READ DATA ####################
+
+
+# Function to handle the /start command:
+# After sending this command, user gets a message how to use bot
+# chat_id is added to the database (kind of user registration)
 @bot.message_handler(commands=['start'])
 def send_start(message):
     try:
         chat_id = message.chat.id
-        bot.send_message(chat_id, "Welcome to Euro 2016 prediction contest! Type /help for help, /rules for rules or /predict to make your predictions")
+        bot.send_message(chat_id,
+                         'Welcome to Euro 2016 prediction contest! ' +
+                         'Type /help for help, ' +
+                         '/rules for rules, or ' +
+                         '/predict to make your predictions')
         db_access.add_user(chat_id)
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Function to handle the /help command:
+# User gets the list of all available commands
 @bot.message_handler(commands=['help'])
 def send_help(message):
     try:
         chat_id = message.chat.id
-        bot.send_message(chat_id, '/start View start message\n' +
-            '/help View this message\n' +
-            '/predict Make prediction\n' +
-            '/setname Set your name\n' +
-            '/setgroup Set your group\n' + 
-            '/rules Show rules\n' +
-            '/today See what happens today\n' +
-            '/ranking See the ranking of your groups\n' +
-            '/current See the predictions for current match')
+        bot.send_message(chat_id,
+                         '/start View start message\n' +
+                         '/help View this message\n' +
+                         '/predict Make prediction\n' +
+                         '/setname Set your name\n' +
+                         '/setgroup Set your group\n' + 
+                         '/rules Show rules\n' +
+                         '/today See what happens today\n' +
+                         '/ranking See the ranking of your groups\n' +
+                         '/current See the predictions for current match')
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Function to handle the /rules command:
+# User gets a short description of how the points are assigned
 @bot.message_handler(commands=['rules'])
 def send_rules(message):
     try:
         chat_id = message.chat.id
-        bot.send_message(chat_id, "You need to answer three questions about each match:\n1. Who will win the game or will there be a draw?\n2. What will be the difference of team goals?\n3. Total of the match?\nYou get 3 points for each correct answer. If all 3 answers are correct, you get an additional point.")
+        bot.send_message(chat_id,
+                         'You need to answer three questions about each match:\n' + 
+                         '1. Who will win the game or will there be a draw?\n' +
+                         '2. What will be the difference of team goals? ' +
+                         '(in case you predicted draw: how many goals will each team score?)\n' +
+                         '3. What will be total of the match? (Over or Under 2.5 golas?)\n' +
+                         'You may get 3 points for each correct answer.' +
+                         ' If all 3 answers are correct, you get an additional point.')
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Function to handle the /today command:
+# User gets a list of matches that will be played today
+# The user also gets his predictions for each match
 @bot.message_handler(commands=['today'])
 def send_today_info(message):
     try:
@@ -74,6 +103,7 @@ def send_today_info(message):
         else:
             user_predictions = user_profile['predictions']
             today_user_prediction_ids = [c['match_no'] for c in user_predictions if c['match_no'] in today_matches_ids]
+            # TODO: make map instead of for-loop:
             for match in today_matches:
                 info_text = info_text + match['team_1'] + " vs. " + match['team_2'] + ': '
                 if match['match_no'] in today_user_prediction_ids:
@@ -99,6 +129,11 @@ def send_today_info(message):
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Function to handle the /current command
+# User gets predictions of members of all his groups
+# for the current matches.
+# Current match is defined as not finished and not active match
 @bot.message_handler(commands=['current'])
 def send_current_match_info(message):
     try:
@@ -110,7 +145,7 @@ def send_current_match_info(message):
         else:
             for match in current_matches:
                 predictions_for_match = db_access.get_predictions_match(match, user_profile)
-                string_to_send = match['team_1'] + " vs. " + match['team_2'] + ':\n'
+                string_to_send = match['team_1'] + ' vs. ' + match['team_2'] + ':\n'
                 for prediction_for_match in predictions_for_match:
                     if ('name' in prediction_for_match):
                         string_to_send = string_to_send + prediction_for_match['name'] + ': '
@@ -124,7 +159,7 @@ def send_current_match_info(message):
                                 string_to_send = string_to_send + match['team_1']
                             elif (prediction['winner'] == 2):
                                 string_to_send = string_to_send + match['team_2']
-                            string_to_send = ' by ' + str(prediction['goals']) + ' goals, '
+                            string_to_send = string_to_send + ' by ' + str(prediction['goals']) + ' goal(s), '
                         if (prediction['total'] == 0):
                             string_to_send = string_to_send + '<2.5\n'
                         elif (prediction['total'] == 1):
@@ -135,19 +170,31 @@ def send_current_match_info(message):
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Help function to create a printable string from group name and ranking list
 def ranking_to_str(group, ranking):
     result = group + '\n'
+    # TODO: add formatting (probably, with additional function)
     for rank in ranking:
         result = result + rank['name'] + '   ' + str(rank['total_points']) + '\n'
     return result
 
+
+# Function to handle the /ranking command
+# The user gets one message per group (where he is a member)
+# This message contains sorted list of group members with their points
+# In case the user is not a member of any group, he may request a group,
+# that he wants to get ranking for
+# (this request is processed in the next function).
 @bot.message_handler(commands=['ranking'])
 def get_ranking(message):
     try:
         chat_id = message.chat.id
         groups = db_access.get_groups(chat_id)
         if (len(groups) == 0):
-            msg = bot.reply_to(message, "You are not a member of any groups! Please enter the name of the group you want to get ranking!")
+            msg = bot.reply_to(message,
+                               'You are not a member of any groups! ' +
+                               'Please enter the name of the group you want to get ranking!')
             bot.register_next_step_handler(msg, process_ranking)
         else:
             user_groups = groups['groups']
@@ -159,6 +206,9 @@ def get_ranking(message):
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
 
+
+# Function called if a user is not a member of any group:
+# Ranking of chosen group is shown.
 def process_ranking(message):
     try:
         chat_id = message.chat.id
@@ -169,12 +219,17 @@ def process_ranking(message):
         msg = bot.reply_to(message, ranking_str)
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
-    
 
+
+#################### FUNCTIONS TO WRITE DATA ####################
+
+
+# Function to handle the /setname command
+# User can set his name
 @bot.message_handler(commands=['setname'])
 def set_name(message):
     try:
-        msg = bot.reply_to(message, "What is your name?")
+        msg = bot.reply_to(message, 'What is your name?')
         bot.register_next_step_handler(msg, process_name)
     except Exception as e:
         bot.reply_to(message, 'Something went wrong! Please, contact the provider of this bot!')
@@ -279,7 +334,7 @@ def process_total_step(message):
     try:
         chat_id = message.chat.id
         pred = pred_dict[chat_id]
-        print("Chat id: " + str(chat_id) + "; Prediction: " + message.text)
+        print('Chat id: ' + str(chat_id) + '; Prediction: ' + message.text)
         if (message.text == '< 2.5'):
             pred.total = 0
         elif (message.text == '> 2.5'):
@@ -294,6 +349,8 @@ def process_total_step(message):
 @bot.message_handler(func=lambda message: True)
 def echo_all(message):
     return (0)
-    # bot.reply_to(message, "Unfortunately we do not support free text. If you see this message, something went wrong (or you just typed some text the bot can not understand).")
+    # bot.reply_to(message, "Unfortunately we do not support free text.
+    # If you see this message, something went wrong
+    # (or you just typed some text the bot can not understand).")
 
 bot.polling()
